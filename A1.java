3bc09b9422c108d5c0ec8c7e6a829083a98cbf49
adhishree1 <96350000+adhishree1@users.noreply.github.com>
2021-12-18 23:46:04 +0530

    Add files via upload

---
+++ A1.java
@@ -0,0 +1,17 @@
+class A1
+{
+	public static void main(String args[])
+	{
+	   int i,j,n=1;
+	   for(i=1;i<=4;i++)
+	   {
+	   	for(j=1;j<=4;j++)
+	   	{
+	   		System.out.print(+n);
+	   	}
+	   	n++;
+	   	System.out.println("");
+	   }	
+       
+	}
+}
